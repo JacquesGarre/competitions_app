d40dedd3fe79786f6d7355f2fae4951eb1d1debc
Jacques Garré <jcqs.gr@gmail.com>
2021-11-14 15:25:56 +0100

    build3

--- front-end/src/app/_globals/env.ts
+++ front-end/src/app/_globals/env.ts
@@ -1,6 +1,6 @@
 export class Env {
     public static APP_NAME: string = 'Smaaash';
-    public static API_URL: string = 'http://146.59.144.212/api/public/api';
+    public static API_URL: string = 'http://146.59.144.212/api/public/api/';
     public static FFTT_API_URL: string = 'https://apiv2.fftt.com/mobile/pxml/xml_licence.php?serie=ADJFJFSQ0545FDS&tm=20190208102245000&tmc=7ce7a41367fbd813e36cf3e218007b95b39bbf75&id=SW459&licence=';
     public static ITEMS_PER_PAGE: any = 15;
 }
